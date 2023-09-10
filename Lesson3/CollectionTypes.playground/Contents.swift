843fa6696f987b265de1ce0dbdd47a08ea5613b8
yakupacs <ykpacs@gmail.com>
2023-09-10 20:56:46 +0300

    Updated.

--- Lesson3/CollectionTypes.playground/Contents.swift
+++ Lesson3/CollectionTypes.playground/Contents.swift
@@ -51,6 +51,7 @@ print(fruits)
 fruits.removeAll()
 print(fruits)
 
+// MARK: - Class Food
 
 class Food
 {
@@ -68,7 +69,7 @@ class Food
 }
 
 var f1 = Food(id: 1, name: "Hamburger", price: 59, rate: 4.7)
-var f2 = Food(id: 2, name: "Big Chicken", price: 139, rate: 3.8)
+var f2 = Food(id: 2, name: "Big Chicken Hamburger", price: 139, rate: 3.8)
 var f3 = Food(id: 3, name: "Süper İkili", price: 89, rate: 4.1)
 
 var foods = [Food]()
@@ -81,10 +82,50 @@ for food in foods
     print("Id: \(food.id!) - Name: \(food.name!) - Price: \(food.price!)₺ - Rate: \(food.rate!)")
 }
 
- // Filter
-var filtered = foods.filter { $0.price! > 100 && $0.rate! > 3.7}
 
+// MARK: - Filter
+
+print("------------------Filtered1------------------")
+var filtered = foods.filter({ $0.price! > 100 && $0.rate! > 3.7})
 for filter in filtered
 {
     print("Filtered -> Id: \(filter.id!) - Name: \(filter.name!) - Price: \(filter.price!)₺ - Rate: \(filter.rate!)")
+}
+
+print("------------------Filtered2------------------")
+var filtered2 = foods.filter({ $0.name!.contains("Hamburger") })
+for filter in filtered2
+{
+    print("Filtered2 -> Id: \(filter.id!) - Name: \(filter.name!) - Price: \(filter.price!)₺ - Rate: \(filter.rate!)")
+}
+
+
+// MARK: - Sorting
+
+print("\n-------------------Sorted Descending-------------------")
+var s1 = foods.sorted(by: { $0.price! > $1.price! }) // Descending: DESC
+for s in s1
+{
+    print("Sorted1  -> Id: \(s.id!) - Name: \(s.name!) - Price: \(s.price!)₺ - Rate: \(s.rate!)")
+}
+
+print("\n-------------------Sorted Ascending-------------------")
+var s2 = foods.sorted(by: { $0.price! < $1.price! }) // Descending: ASC
+for s in s2
+{
+    print("Sorted2 -> Id: \(s.id!) - Name: \(s.name!) - Price: \(s.price!)₺ - Rate: \(s.rate!)")
+}
+
+print("\n-------------------Rate Descending-------------------")
+var r1 = foods.sorted(by: { $0.rate! > $1.rate! }) // Descending: DESC
+for s in s1
+{
+    print("Sorted3  -> Id: \(s.id!) - Name: \(s.name!) - Price: \(s.price!)₺ - Rate: \(s.rate!)")
+}
+
+print("\n-------------------Rate Ascending-------------------")
+var r2 = foods.sorted(by: { $0.rate! < $1.rate! }) // Descending: ASC
+for s in s2
+{
+    print("Sorted4 -> Id: \(s.id!) - Name: \(s.name!) - Price: \(s.price!)₺ - Rate: \(s.rate!)")
 }
