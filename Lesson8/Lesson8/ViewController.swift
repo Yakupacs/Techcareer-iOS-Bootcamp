86f60b35eb42c3fb7105924182cac8c449fcf214
yakupacs <ykpacs@gmail.com>
2023-09-21 18:04:06 +0300

    Updated.

--- Lesson8/Lesson8/ViewController.swift
+++ Lesson8/Lesson8/ViewController.swift
@@ -9,11 +9,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
 
-
+	@IBAction func alertAction(_ sender: Any) {
+		let alert = UIAlertController(title: "Warning!", message: "Username or password is incorrect!", preferredStyle: .alert)
+		
+		let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) { _ in
+			print("Cancel Action Clicked!")
+		}
+		let okAction = UIAlertAction(title: "Okey", style: .default) { _ in
+			print("Okey Action Clicked!")
+		}
+		
+		alert.addAction(cancelAction)
+		alert.addAction(okAction)
+		
+		self.present(alert, animated: true)
+	}
+	
+	@IBAction func actionSheetAction(_ sender: Any) {
+		let alert = UIAlertController(title: "Warning!", message: "Username or password is incorrect!", preferredStyle: .actionSheet)
+		
+		let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) { _ in
+			print("Cancel Action Clicked!")
+		}
+		let okAction = UIAlertAction(title: "Okey", style: .default) { _ in
+			print("Okey Action Clicked!")
+		}
+		
+		alert.addAction(cancelAction)
+		alert.addAction(okAction)
+		
+		self.present(alert, animated: true)
+	}
+	
+	@IBAction func speacialAlertAction(_ sender: Any) {
+		let alert = UIAlertController(title: "System Login", message: "Email and Password required!", preferredStyle: .alert)
+		
+		alert.addTextField() { textField in // Index: 0
+			textField.placeholder = "Email"
+			textField.keyboardType = .emailAddress
+		}
+		
+		alert.addTextField() { textField in // Index: 1
+			textField.placeholder = "Password"
+			textField.keyboardType = .numberPad
+			textField.isSecureTextEntry = true
+		}
+		
+		let loginAction = UIAlertAction(title: "Login", style: .cancel){ _ in
+			let txtFieldEmail = alert.textFields![0] as UITextField
+			let txtFieldPassword = alert.textFields![1] as UITextField
+			
+			if let email = txtFieldEmail.text, let password = txtFieldPassword.text {
+				print("Email: \(email), Password: \(password)")
+			}
+		}
+		
+		alert.addAction(loginAction)
+		
+		self.present(alert, animated: true)
+	}
+	
 }
 
