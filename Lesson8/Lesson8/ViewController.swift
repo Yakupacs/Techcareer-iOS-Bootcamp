3012bbbb368972f5f5be3e0602eb0b6a03088d8d
yakupacs <ykpacs@gmail.com>
2023-09-21 17:48:01 +0300

    Updated.

---
+++ Lesson8/Lesson8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson8
+//
+//  Created by Yakup on 20.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
