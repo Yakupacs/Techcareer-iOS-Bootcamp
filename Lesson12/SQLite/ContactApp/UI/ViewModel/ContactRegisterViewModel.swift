805a78257d0d33170868960a48d27daf1e8f77ce
yakupacs <ykpacs@gmail.com>
2023-10-01 14:17:11 +0300

    Updated.

--- Lesson12/SQLite/ContactApp/UI/ViewModel/ContactRegisterViewModel.swift
+++ Lesson12/SQLite/ContactApp/UI/ViewModel/ContactRegisterViewModel.swift
@@ -10,7 +10,6 @@ import Foundation
 class ContactRegisterViewModel{
 	var krepo = ContactDaoRepository()
 	
-	
 	func save(id: Int, name: String, surname: String, phone: String)
 	{
 		krepo.save(id: id, name: name, surname: surname, phone: phone)
